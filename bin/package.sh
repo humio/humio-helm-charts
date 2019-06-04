07bb8c20209352e545dec28a945564285b12dc62
Jestin Woods <jestin@humio.com>
2019-06-04 13:18:49 -0700

    Properly package the helm charts

--- bin/package.sh
+++ bin/package.sh
@@ -2,7 +2,14 @@
 
 set -ex
 
+# Remove any existing indexes
 find . -iname index.yaml | grep -v docs | xargs rm
+
+# Move existing releases to the charts directory so they can be re-indexed with proper URLs
+find docs/ -name '*.tgz' -maxdepth 1 -exec mv {} ./ \;
+
 helm package .
-rm docs/*.tgz && mv *.tgz docs/
-helm repo index . && mv index.yaml docs/
+helm repo index . --url / && mv index.yaml docs/
+
+# Move all the charts to the docs so they can be served by github pages
+find . -name '*.tgz' -mindepth 1 -maxdepth 1 -exec mv {} docs/ \;
